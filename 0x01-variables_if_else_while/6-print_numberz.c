356cfa9f5f3e44b42e165221cebbb648612be7fa
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-06-03 16:44:10 -0700

    six

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print all single digit numbers of base ten starting from zero
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		putchar( c + '0');
+	}
+	putchar('\n');
+	return (0);
+}
