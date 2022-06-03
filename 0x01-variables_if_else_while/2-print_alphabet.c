e18bdd0b0892dfef0ef4ce34643fc648f7ca9d62
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-06-03 15:45:39 -0700

    two

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - printing of alphabets in lower case
+ * 
+ * Return: return 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+return (0);
+}
