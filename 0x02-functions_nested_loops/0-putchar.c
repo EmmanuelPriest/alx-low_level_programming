ae844807febc4ac32f642d8633f6af95eaa4abfa
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-06-07 16:47:09 -0700

    zero

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+/**
+ * main - program that prints _putchar
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	char c[] = "_putchar";
+	int i;
+
+	i = strlen(c);
+
+	for (i = 0; i < i; i++)
+	{
+		_putchar(c[i]);
+	}
+
+	_putchar('\n');
+
+return (0);
+}
