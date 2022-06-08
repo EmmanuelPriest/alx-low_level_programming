0dd007aaa2cc0114016db0de848a990de7ad165d
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-06-07 17:31:16 -0700

    one

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdbool.h>
+/**
+ * main - function that prints the alphabet in lowercase
+ *
+ */
+
+void print_alphabet(void)
+{
+	int i = 97;
+	bool a = true;
+
+	while (a)
+	{
+		_putchar(i);
+
+		if (i == 122)
+		{
+			a = false;
+		}
+		i++;
+	}
+	_putchar('\n');
+}
