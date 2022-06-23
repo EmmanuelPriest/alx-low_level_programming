4ff21f1702d2bed1e9066f71736489972af472dc
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-06-22 20:42:38 -0700

    function tat compares two strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,69 +1,22 @@
 #include "main.h"
 
-int bandersnatch(char *s1, char *s2);
-char *move(char *s2);
 /**
  * wildcmp - function that compares two strings
  * @s1: first compared string
  * @s2: second compared string
- * Return: 1 if the strings can be considered identical otherwise 0
+ * Return: 1 if the strings can be considered identical, otherwise 0
  */
 int wildcmp(char *s1, char *s2)
 {
-	int sum = 0;
-
-	if (*s1 == '\0' && *s2 == '*' && !*move(s2))
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+		return (0);
+	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-
 	if (*s1 == *s2)
-	{
-		if (*s1 == '\0')
-			return (1);
 		return (wildcmp(s1 + 1, s2 + 1));
-	}
-
-	if (*s1 == '\0' || *s2 == '\0')
-		return (0);
-
 	if (*s2 == '*')
-	{
-		s2 = move(s2);
-		if (*s2 == '\0')
-			return (1);
-		if (*s1 ==  *s2)
-			sum += wildcmp(s1 + 1, s2 + 1);
-		sum += bandersnatch(s1 + 1, s2);
-		return (!!sum);
-	}
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	return (0);
-}
-
-/**
- * bandersnatch - function that checks for all the paths when equal char
- * @s1: first string
- * @s2: second string
- * Return: value of wildcmp() or itself
- */
-int bandersnatch(char *s1, char *s2)
-{
-	if (*s1 == '\0')
-		return (0);
-	if (*s1 == *s2)
-		return (wildcmp(s1, s2));
-	return (bandersnatch(s1 + 1, s2));
-}
-
-/**
- * *move - function that moves the current chararater pass the *
- * @s2: string to work on
- * Return: the address of character after the *
- */
-char *move(char *s2)
-{
-	if (*s2 == '*')
-		return (move(s2 + 1));
-	else
-		return (s2);
 }
 
 
