a93ac8cebe3eee57e2318a33db732ebb7098afb0
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-07-15 16:27:09 -0700

    function that prints something before the main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
+void print_something(void) __attribute__ ((constructor));
+
 /**
-* main - function that prints a message before the main
+* print_something - function that prints a message before the main
 *
 * Return: nothing
 */
-int main(void)
+void print_something(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
 			"I bore my house upon my back!\n");
-	return (0);
 }
