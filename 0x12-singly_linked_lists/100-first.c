91a1b3cc8d2045bd36226d8862564a67dd4ede29
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-07-15 16:18:06 -0700

    function that prints something before the main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - function that prints a message before the main
+*
+* Return: nothing
+*/
+int main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+	return (0);
+}
