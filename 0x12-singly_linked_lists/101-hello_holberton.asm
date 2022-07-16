72119c57a08a556d89e2d55f2f835a8d8e88f282
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-07-15 17:12:53 -0700

    64-bit program in assembly that prints Hello, Holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,12 +1,13 @@
-extern printf			; the C function to be called
+section .data			; Data section, initialized variable
 
-	section .data		; Data section, initialized variables
-msg:	db "Hello, holberton", 0; C string that needs 0
 fmt:	db "%s", 10, 0		; The printf format, "\n", '0'
+msg:    db "Hello, holberton", 0; C string that needs 0
+
 
 	section .text		; Code section
-
+	extern printf		; The C function to be called
 	global main		; The standard gcc entry point
+
 main:				; The prgram label for the entry point
 	push	rbp		; Set up stack frame, must be alligned
 
