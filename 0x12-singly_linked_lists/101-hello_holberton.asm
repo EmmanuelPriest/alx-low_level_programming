1ff0704df4d3f21cf7be3f766cf043f90319f398
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-07-17 04:12:05 -0700

    64-bit program in assembly that prints Hello, Holberton, followed by a new line

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,20 @@
-SECTION .data                   ; Data section, initialized variable
-msg:    db "Hello, holberton", 0; C string that needs 0
-fmt:    db "%s", 10, 0          ; The printf format, "\n", '0'
+SECTION .data
+msg:    db "Hello, holberton", 0
+fmt:    db "%s", 10, 0
 
-	SECTION .text           ; Code section
-	extern printf		; The C function to be called
-	global main             ; The standard gcc entry point
+	SECTION .text
+	extern printf
+	global main
 
-main:				; The prgram label for the entry point
-	push rbp		; Set up stack frame, must be alligned
+main:
+	push rbp
 
 	mov rsi, msg
 	mov rdi, fmt
-	mov rax, 0		; Or can be xor rax,rax
-	call printf		; call C function
+	mov rax, 0
+	call printf
 
-	pop rbp			; restore stack
+	pop rbp
 
-	mov rax, 0		; normal, no error, return value
-	ret			; return
+	mov rax, 0
+	ret
