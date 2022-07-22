bea10e03cbeed9d12f2f7b2f8922a2f1379eb489
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-07-22 05:30:23 -0700

    program that prints a string of characters without using printf and puts functions

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
- * main - Entry point
- * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
- * Return: Always 0 (Success)
- */
+* main - program that prints a string of charaters to the std error
+*
+* Return: 1
+*/
 int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-return (1);
+	return (1);
 }
