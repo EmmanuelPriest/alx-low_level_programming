dd39508a63e97746603c94029fbdddcee8a30eb2
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-07-24 13:52:40 -0700

    program that prints all possible different combinations of 3 digits considering 012, 120, 102, 021, 201 and 210 as the same combination, using putchar

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,43 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - Entry point
- * description: print all possible different combinations of three digits
- * Return: Always 0 (Success)
- */
-
+* main - function that prints all possible different combinations of 3 digits
+*
+* Return: Always 0(Success)
+*/
 int main(void)
 {
-int n;
-int a;
-int x = 0;
+	int n, m, x;
 
-while (x < 10)
-{
-a = 0;
-while (a < 10)
-{
-n = 0;
-while (n < 10)
-{
-if (n != a && a != x && x < a && a < n)
-{
-putchar('0' + x);
-putchar('0' + a);
-putchar('0' + n);
-if (n + a + x != 9 + 8 + 7)
-{
-putchar(',');
-putchar(' ');
-}
-}
-n++;
-}
-a++;
-}
-x++;
-}
-putchar('\n');
-return (0);
+	for (n = 48; n <= 57; n++)
+	{
+		for (m = 49; m <= 57; m++)
+		{
+			for (x = 50; x <= 57; x++)
+				if (x > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(x);
+					if (n < 55 || m < 56 || x < 57)
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+		}
+	}
+	putchar('\n');
+
+	return (0);
 }
