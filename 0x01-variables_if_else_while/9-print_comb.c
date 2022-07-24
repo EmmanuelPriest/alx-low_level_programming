ca5c49f1bbe43fbe0187e5e8ad7f82a3b9701d13
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-07-24 08:59:06 -0700

    program that prints all possible combinations of single-digit numbers using putchar

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,12 +10,14 @@ int main(void)
 	int n;
 
 	n = 48;
-	while (n <= 57)
+	for (n = 48; n <= 57; n++)
 	{
 		putchar(n);
-		putchar(44);
-		putchar(32);
-		n++;
+		if (n < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
 	}
 	putchar('\n');
 
