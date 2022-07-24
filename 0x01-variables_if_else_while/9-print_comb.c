2a340c53fffde55962ff0af0a8cf4196b8da43ef
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-07-24 08:27:45 -0700

    program that prints all possible combinations of single-digit numbers using putchar

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,29 +1,27 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
- * main - print all possible combinations of single-digit numbers
- *
- * Return: return 0
- */
+* main - function that prints all possible combinations of single-digit number
+*
+* Return: Always 0(Success)
+*/
 int main(void)
 {
-	int i;
+	int n;
 
-	for (i = 0; i < 10; i++)
+	n = 48;
+	while (n <= 57)
 	{
-		putchar(i + '0');
-
-		if (i == 9)
-		{
-		}
-		else
-		{
-			putchar(',');
-			putchar(' ');
-		}
+		putchar(n);
+		putchar(44);
+		putchar(32);
+		n++;
 	}
 	putchar('\n');
+
 	return (0);
 }
+/*
+* The ascii values of 0, 9, comma and space are 48, 57, 44 and 32
+* respectively
+*/
