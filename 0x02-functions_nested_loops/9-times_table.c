e58db10c40987341d5acea1891e71f243d0d1f13
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-07-28 14:27:09 -0700

    program that prints the 9 times table in matrix form, starting with 0, using _putchar

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,37 +1,39 @@
 #include "main.h"
 /**
- * times_table - prints the 9 times table starting with 0
- *
- */
+* times_table - prints the 9 times table, in matrix form, starting with 0
+*
+* Return: nothing
+*/
 void times_table(void)
 {
-int row;
-int column;
-int value;
-for (row = 0; row <= 9; row++)
-{
-for (column = 0; column <= 9; column++)
-{
-value = (row * column);
-if (column == 0)
-{
-_putchar('0' + value);
-}
-else if (value <= 9)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar('0' + value);
-}
-else if (value > 9)
-{
-_putchar(',');
-_putchar(' ');
-_putchar('0' + (value / 10));
-_putchar('0' + (value % 10));
-}
-}
-_putchar('\n');
-}
+	int row, column, product;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			product = row * column;
+
+			if (column == 0)
+			{
+				_putchar(product + '0');
+			}
+
+			else if (product <= 9)
+			{
+				_putchar(44);
+				_putchar(46);
+				_putchar(46);
+				_putchar(product + '0');
+			}
+			else if (product > 9)
+			{
+				_putchar(44);
+				_putchar(46);
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
