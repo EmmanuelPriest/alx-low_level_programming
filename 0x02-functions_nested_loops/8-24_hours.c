5ed974a1cdcc8a98a7835d5d26399d75b7c10478
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-07-28 08:26:42 -0700

    program that prints every minute of the day starting from 00:00 to 23:59 using _putchar

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,29 +1,27 @@
 #include "main.h"
-/**
- * jack_bauer - printf every second of the day in 00:00 format
- *
- * Return: there is no return just print to the console.
- */
 
+/**
+* jack_bauer - function that prints every minute of the day starting
+* from 00:00 to 23:59
+*
+* Return: nothing
+*/
 void jack_bauer(void)
 {
-	int i, j, h1, h2, m1, m2;
+	int n = 0, m = 0;
 
-	for (i = 0; i < 24; ++i)
+	while (n < 24)
 	{
-		for (j = 0; j < 60; ++j)
+		while (m < 60)
 		{
-			h1 = i / 10 + '0';
-			h2 = i % 10 + '0';
-			m1 = j / 10 + '0';
-			m2 = j % 10 + '0';
-			_putchar(h1);
-			_putchar(h2);
-			_putchar(':');
-			_putchar(m1);
-			_putchar(m2);
+			_putchar((n / 10) + 48);
+			_putchar((n % 10) + 48);
+			_putchar(58);
+			_putchar((m / 10) + 48);
+			_putchar((m % 10) + 48);
+			m++;
 			_putchar('\n');
 		}
+		n++;
 	}
-
 }
