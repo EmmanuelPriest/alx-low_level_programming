246abea0cdc060effb9e303e2b01146cc873a344
EmmanuelPiest <emmanuelchukwujekwu87@gmail.com>
2022-08-20 08:10:01 -0700

    function that prints half of a string(RHS/second half) using _putchar

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,19 +1,26 @@
 #include "main.h"
 #include <string.h>
 /**
- * puts_half - function that prints half of a string
- * @str: string parameter
- */
+* puts_half - function that prints half of a string(second half of the string)
+* @str: char string pointer
+*
+* Return: nothing
+*/
 void puts_half(char *str)
 {
-	int m, n, i;
+	int length, mid, i;
 
-	m = strlen(str);
-	if (m % 2 == 1)
-		n = m / 2 + 1;
+	/* getting the length of str */
+	length = strlen(str);
+	/* checking if length of str is odd */
+	if (length % 2 == 1)
+		/* RHS/ second half of str */
+		mid = length / 2 + 1;
 	else
-		n = m / 2;
-	for (i = n; i < m; i++)
+		/* LHS/ first half of str */
+		mid = length / 2;
+	/* printing the RHS(second half) of str */
+	for (i = mid; i < length; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }
