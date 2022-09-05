2f8053dc614f60eed9d5a14f636167b81afd3aa4
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-09-05 09:58:49 -0700

    coming

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+
+int add(a, b)
+int sub(a, b)
+int mul(a, b)
+int div(a, b)
+#include <stdio.h>
+
+int add(a, b)
+int sub(a, b)
+int mul(a, b)
+int div(a, b)
+
+/**
+* add - function that adds 2 ints
+* @a: int
+* @b: int
+*
+* Return: addition of a and b
+*/
+int add(a, b)
+{
+        return (a + b);
+}
+
+/**
+* sub - function that substracts 2 ints
+* @a: int
+* @b: int
+-- INSERT --                                                  7,1           Top
+/**
+* add - function that adds 2 ints
+* @a: int
+* @b: int
+*
+* Return: addition of a and b
+*/
+int add(a, b)
+{
+	return (a + b);
+}
+
+/**
+* sub - function that substracts 2 ints
+* @a: int
+* @b: int
+*
+* Return: substraction of a and b
+*/
+int sub(a, b)
+{
+	return (a - b);
+}
+
+/**
+* mul - function that multiplies 2 ints
+* @a: int
+* @b: int
+*
+* Return: multiplication of a and b
+*/
+int mul(a, b)
+{
+	return (a * b);
+}
+
+/**
+* div - function that divides 2 ints
+* @a: int
+* @b: int
+*
+* Return: division of a and b
+*/
+int div(a, b)
+{
+	return (a / b);
+}
+
+/**
+* mod - function that returns remainder of 2 int division
+* @a: int
+* @b: int
+*
+* Return: modulo of a and b
+*/
+int mod(a, b)
+{
+	return (a % b);
+}
