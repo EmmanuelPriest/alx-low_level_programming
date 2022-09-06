dc8de7cfe9791673db5fb96453a1d6705be645c2
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-09-06 03:48:12 -0700

    correction

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -q -O /tmp/sheetmode.so https://github.com/EmmanuelPriest/alx-low_level_programming/raw/master/0x18-dynamic_libraries/sheetmode.so
-export LD_PRELOAD=/tmp/sheetmode.so
+LD_PRELOAD=./sheetmode.so
