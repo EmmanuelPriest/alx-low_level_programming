1a756ef87583082748cc50c0852891fccef6caca
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-09-06 05:20:50 -0700

    giga million

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/nrandom.so https://github.com/EmmanuelPriest/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=./nrandom.so
+wget -P /tmp/sheetmode.so https://github.com/EmmanuelPriest/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=./sheetmode.so
